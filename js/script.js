a3241c8c51db48e90aa2cf6379d790f3571d8f1f
HotBun <goooneoooogle@gmail.com>
2019-04-17 11:51:28 +0300

    ToDoIst v0.3 - Themes ready

--- js/script.js
+++ js/script.js
@@ -2,17 +2,74 @@
 
 let showProjects = document.querySelector("#projects .title_block"), // Шапка проектов
     projects = document.getElementById("projects"), // Общий блок проектов
-    projectContain = document.querySelector(".project_contain"); // Список проектов
+    projectContain = document.querySelector(".project_contain"), // Контейнер проектов
+
+    showFilters = document.querySelector("#filters .title_block"), // Шапка фильтров
+    filters = document.getElementById("filters"), // Общий блок фильтров
+    filtersContain = document.querySelector(".filters_contain"), //  Контейнер фльтров
+
+    showThemes = document.querySelector("#themes .title_block"), // Шапка тем
+    themes = document.getElementById("themes"), // Общий блок тем
+    themesContain = document.querySelector(".themes_contain"), // Контейнер тем
+    themesElem = document.querySelectorAll(".themes_elem"),
+
+    myBody = document.querySelector("body"), // Список тем
+
+    howMutchTasks = document.querySelector(".how_muth span"), // Сколько всего задач
+    youTheme = document.querySelector(".you_theme span"); // Текущая тема
+
+
+let orangeTheme = document.getElementById("orange"),
+    darkTheme = document.getElementById("dark");
+
 
 
 // Показать список проектов
 showProjects.addEventListener("click", () => {
-    if (projects.classList.contains('project_expanded')) {
-        projects.classList.remove("project_expanded");
-        projectContain.style.cssText = "height:0; overflow: hidden; transition-duration: 335ms;";
+    if (projects.classList.contains('expanded')) {
+        projects.classList.remove("expanded");
     } else {
-        projects.classList.add("project_expanded");
-        projectContain.style.cssText = "height:auto; overflow: visible; transition-duration: 335ms;";
+        projects.classList.add("expanded");
     }
+});
 
+// Показать список фильтров
+showFilters.addEventListener("click", () => {
+    if (filters.classList.contains('expanded')) {
+        filters.classList.remove("expanded");
+    } else {
+        filters.classList.add("expanded");
+    }
+});
+
+// Показать список тем
+showThemes.addEventListener("click", () => {
+    if (themes.classList.contains('expanded')) {
+        themes.classList.remove("expanded");
+    } else {
+        themes.classList.add("expanded");
+    }
+});
+
+// Переключечние тем
+themesElem.forEach(function (item) {
+    item.addEventListener("click", () => {
+        myBody.className = "";
+        if (item.id == "dark") {
+            myBody.classList.add("dark");
+            youTheme.textContent = myBody.className;
+        } else if (item.id == "orange") {
+            myBody.classList.add("orange");
+            youTheme.textContent = myBody.className;
+        } else if (item.id == "green") {
+            myBody.classList.add("green");
+            youTheme.textContent = myBody.className;
+        } else if (item.id == "blue") {
+            myBody.classList.add("blue");
+            youTheme.textContent = myBody.className;
+        } else if (item.id == "white") {
+            myBody.classList.add("white");
+            youTheme.textContent = myBody.className;
+        }
+    });
 });
